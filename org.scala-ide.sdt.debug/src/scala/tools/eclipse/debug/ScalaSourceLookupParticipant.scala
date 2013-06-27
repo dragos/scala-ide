0cb70cb2c44b34620f5caa02efd4dbe06bcfc845
Iulian Dragos <jaguarul@gmail.com>
2013-06-27 18:21:52 +0200

    Better UI and practically working.

--- org.scala-ide.sdt.debug/src/scala/tools/eclipse/debug/ScalaSourceLookupParticipant.scala
+++ org.scala-ide.sdt.debug/src/scala/tools/eclipse/debug/ScalaSourceLookupParticipant.scala
@@ -2,6 +2,7 @@ package scala.tools.eclipse.debug
 
 import org.eclipse.debug.core.sourcelookup.AbstractSourceLookupParticipant
 import scala.tools.eclipse.debug.model.ScalaStackFrame
+import scala.tools.eclipse.debug.async.AsyncStackFrame
 
 /**
  * SourceLookupParticipant providing a source names when using the
@@ -13,6 +14,8 @@ object ScalaSourceLookupParticipant extends AbstractSourceLookupParticipant {
     obj match {
       case stackFrame: ScalaStackFrame =>
         stackFrame.getSourcePath
+      case sf: AsyncStackFrame =>
+        sf.getSourcePath
       case _ =>
         null
     }
