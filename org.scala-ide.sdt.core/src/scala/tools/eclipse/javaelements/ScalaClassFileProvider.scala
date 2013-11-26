82a57e4a9d075ce08f15796a2522bc8b75ba0648
Iulian Dragos <jaguarul@gmail.com>
2013-11-26 14:22:58 +0000

    Cache info about jar files that might contain Scala classifies.
    Deciding wether a class file is produced by Scala is expensive (it entails
    parsing the binary). In this commit we cache the information per package fragment,
    assuming that a jar contains Scala-only or Java-only classifies.(cherry picked from commit e8e791e2e3a274f62948d67260856f3c1c77bf99)
    
    Conflicts:
    	org.scala-ide.sdt.core/src/scala/tools/eclipse/javaelements/ScalaClassFileProvider.scala

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/javaelements/ScalaClassFileProvider.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/javaelements/ScalaClassFileProvider.scala
@@ -7,31 +7,61 @@ package scala.tools.eclipse
 package javaelements
 
 import java.io.ByteArrayInputStream
-import org.eclipse.jdt.internal.core.{ ClassFile, PackageFragment }
-import scala.tools.eclipse.contribution.weaving.jdt.cfprovider.IClassFileProvider
+
+import scala.collection.immutable
 import scala.tools.eclipse.ScalaClassFileDescriber
-import org.eclipse.jdt.core.IClassFile
+import scala.tools.eclipse.ScalaPlugin
+import scala.tools.eclipse.contribution.weaving.jdt.cfprovider.IClassFileProvider
 import scala.tools.eclipse.logging.HasLogger
 
+import org.eclipse.jdt.core.IClassFile
+import org.eclipse.jdt.core.IJavaElement
+import org.eclipse.jdt.core.IPackageFragmentRoot
+import org.eclipse.jdt.internal.core.ClassFile
+import org.eclipse.jdt.internal.core.PackageFragment
+
 class ScalaClassFileProvider extends IClassFileProvider with HasLogger {
-  override def create(contents : Array[Byte], parent : PackageFragment, name : String) : ClassFile =
-    ScalaClassFileDescriber.isScala(new ByteArrayInputStream(contents)) match {
-      case Some(sourceFile) =>
-        val scf = new ScalaClassFile(parent, name, sourceFile)
-        val sourceMapper = parent.getSourceMapper
-        if (sourceMapper == null)
-          null
-        else {
-          val source = sourceMapper.findSource(scf.getType, sourceFile)
-          if (source != null) scf else null
-        }
-      case _ => null
+
+  override def create(contents: Array[Byte], parent: PackageFragment, name: String): ClassFile = {
+    def updateCache(isScalaClassfile: Boolean) {
+      val pfr = parent.getPackageFragmentRoot()
+      if ((pfr ne null) && !scalaPackageFragments.isDefinedAt(pfr)) {
+        logger.debug(s"Setting ${pfr.getElementName} (becasue of class $name) to be ${if (isScalaClassfile) "Scala" else "Java"}")
+        synchronized { scalaPackageFragments += pfr -> isScalaClassfile }
+      }
     }
-  
-  override def isInteresting(classFile: IClassFile): Boolean = {
-    val res = ScalaPlugin.plugin.isScalaProject(classFile.getJavaProject())
-    if (!res) 
-      logger.debug("Not interested in %s in project %s".format(classFile.getElementName(), classFile.getJavaProject().getElementName()))
-    res
+
+    val scalaCF = ScalaClassFileDescriber.isScala(new ByteArrayInputStream(contents)) match {
+      case Some(sourcePath) => new ScalaClassFile(parent, name, sourcePath)
+      case _                => null
+    }
+    updateCache(scalaCF ne null)
+    scalaCF
   }
+
+  /** Return `true` if the classfile could be a Scala classfile.
+   *
+   *  @note This method caches the result of the first classfile read from a package fragment (usually a jar).
+   *        This heuristic might fail if a single jar mixes Java and Scala classfiles.
+   */
+  override def isInteresting(classFile: IClassFile): Boolean = {
+    if (ScalaPlugin.plugin.isScalaProject(classFile.getJavaProject)) {
+      val pfr = ancestorFragmentRoot(classFile)
+      val stillInteresting = pfr.map(scalaPackageFragments.getOrElse(_, true)).getOrElse(true)
+      if (!stillInteresting)
+        logger.debug(s"Cached result for ${classFile.getElementName}: ${pfr.map(_.getElementName())}. is a Java jar")
+
+      stillInteresting
+    } else
+      false
+  }
+
+  private def ancestorFragmentRoot(classFile: IClassFile): Option[IPackageFragmentRoot] =
+    classFile.getAncestor(IJavaElement.PACKAGE_FRAGMENT_ROOT) match {
+      case pfr: IPackageFragmentRoot => Some(pfr)
+      case _                         => None
+    }
+
+  @volatile
+  private var scalaPackageFragments: Map[IPackageFragmentRoot, Boolean] = new immutable.HashMap()
 }
